10646090952cf33d3208bf2ab321ab9902c7cd8d
cmkmchase <cmkmchase@gmail.com>
2026-04-15 04:52:49 +0000

    COLOR UPDATES css

--- src/react-app/components/navbar.tsx
+++ src/react-app/components/navbar.tsx
@@ -3,15 +3,15 @@ import { Link } from "react-router-dom";
 function Navbar() {
   return (
     <header className="navbar">
-      <div className="container navbar-inner" color="white">
+      <div className="container navbar-inner">
         <Link to="/" className="brand hit-and-run">
           Groundworks
         </Link>
 
         <nav className="nav-links">
-          <Link to="/services" className="placard" color="white">Services</Link>
-          <Link to="/about" className="placard" color="white">About</Link>
-          <Link to="/case-studies" className="placard" color="white">Case Studies</Link>
+          <Link to="/services" className="placard">Services</Link>
+          <Link to="/about" className="placard">About</Link>
+          <Link to="/case-studies" className="placard">Case Studies</Link>
           <Link to="/contact" className="placard button button-secondary">
             Contact
           </Link>
